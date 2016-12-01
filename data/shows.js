c774535a4f2ede8f3c5011cdc3cfb3976f21caf1
Nick Choi <iinikchi@gmail.com>
2016-12-01 15:29:12 -0500

    also

--- data/shows.js
+++ data/shows.js
@@ -1,34 +1,33 @@
 const mongoCollections = require("../config/mongoCollections");
-const reviews = mongoCollections.reviews;
+const shows = mongoCollections.shows;
 const users = require("./users");
 const uuid = require('node-uuid');
 
 let exportedMethods = {
-  getAllReviews() {
-    return reviews().then((reviewsCollection) => {
-      return reviewsCollection.find({}).toArray();
+  getAllShows() {
+    return shows().then((showCollection) => {
+      return showCollection.find({}).toArray();
     })
   },
-  getReviewsById(id) {
-    return reviews().then((reviewsCollection) => {
-      return reviewsCollection.findOne({ _id: id }).then((reviews) => {
-        if (!reviews) throw "Review not found";
-        return reviews;
+  getShowById(id) {
+    return shows().then((showCollection) => {
+      return showCollection.findOne({ _id: id }).then((show) => {
+        if (!show) throw "Review not found";
+        return show;
       });
     });
   },
-  //WORK ON THIS
-  addPost(title, body, posterId) {
+  addShow(name, description, thumbnail, preview, rating, reviews) {
     return reviews().then((reviewsCollection) => {
       return users.getUserById(posterId)
         .then((userThatPosted) => {
-          let newPost = {
-            title: title,
-            body: body,
-            poster: {
-              id: posterId,
-              name: `${userThatPosted.firstName} ${userThatPosted.lastName}`
-            },
+          let newShow = {
+            name: name,
+            description: description,
+            thumbnail: thumbnail,
+            preview: preview,
+            rating: rating,
+            reviews: [],
             _id: uuid.v4()
           };
 
@@ -40,33 +39,13 @@ let exportedMethods = {
         });
     });
   },
-  removeReviews(id) {
-    return reviews().then((reviewsCollection) => {
-      return reviewsCollection.removeOne({ _id: id }).then((deletionInfo) => {
+  removeShow(id) {
+    return shows().then((showCollection) => {
+      return showCollection.removeOne({ _id: id }).then((deletionInfo) => {
         if (deletionInfo.deletedCount === 0) {
           throw (`Could not delete post with id of ${id}`)
         } else { }
       });
-    });
-  },
-  //WORK ON THIS
-  updatePost(id, title, body, posterId) {
-    return posts().then((postCollection) => {
-      return users.getUserById(posterId)
-        .then((userThatPosted) => {
-          let updatedPost = {
-            title: title,
-            body: body,
-            poster: {
-              id: posterId,
-              name: userThatPosted.name
-            }
-          };
-
-          return postCollection.updateOne({ _id: id }, updatedPost).then((result) => {
-            return this.getPostById(id);
-          });
-        });
     });
   }
 }
