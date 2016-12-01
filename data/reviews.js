5d1264abc68188a0b1e6b59e59acea139bfdf5e8
Nick Choi <iinikchi@gmail.com>
2016-12-01 15:48:51 -0500

    reviews

---
+++ data/reviews.js
@@ -0,0 +1,11 @@
+const mongoCollections = require("../config/mongoCollections");
+const shows = mongoCollections.shows;
+const users = require("./users");
+const uuid = require('node-uuid');
+
+let exportedMethods = {
+  
+}
+
+
+module.exports = exportedMethods;
