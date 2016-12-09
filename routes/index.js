348f67de099f2d6911762c444a2757c145df9d58
Jeremy Bassi <bassij@thingee.com>
2016-12-08 19:15:03 -0500

    add 404, remove extra readme

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const userRoutes = require("./users");
 const showsRoutes = require("./shows");
+const path = require("path");
 
 const constructorMethod = (app) => {
     app.use("/users", userRoutes);
     app.use("/shows", showsRoutes);
 
     app.use("*", (req, res) => {
-        res.redirect("/shows");
-    })
+      let route = path.resolve("static/404.html");
+      res.status(404);
+      res.sendFile(route);
+    });
 };
 
 module.exports = constructorMethod;
