25f6a988fc0359280740eae6cb1b72049774f689
Jeremy Bassi <bassij@thingee.com>
2016-12-11 12:57:44 -0500

    add passport fcts to user data

--- data/users.js
+++ data/users.js
@@ -1,6 +1,8 @@
 const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
 const uuid = require('node-uuid');
+const bcrypt = require('bcrypt-nodejs');
+
 /*
 {
 	_id:1111-2222-3333-4444
@@ -12,9 +14,48 @@ const uuid = require('node-uuid');
 }
 */
 let exportedMethods = {
+  authenticate(username, password, done) {
+    getUserByUsername(username).then((user) => {
+      checkPassword(user, password).then((res) => {
+        return done(null, user);
+      }).catch((e) => {
+        return done(null, false, { message: 'Incorrect password' });
+      });   
+    }).catch((e) => {
+      return done(null, false, { message: 'Incorrect username' });
+    });
+  },
+  serializeUser(user, done) {
+    done(null, user._id);
+  },
+  deserializeUser(id, done) {
+    getUserById(id).then((user) => {
+      done(null, user);
+    }).catch((e) => {
+      done(e, null);
+    })
+  },
+  checkPassword(user, password) {
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+      if (res)
+        resolve(true);
+      else
+        reject(err);
+      });
+    });
+  },
   getAllUsers() {
     return users().then((userCollection) => {
       return userCollection.find({}).toArray();
+    });
+  },
+  getUserByUsername(username) {
+    return users().then((userCollection) => {
+      return userCollection.findOne({ username: username }).then((user) => {
+        if (!user) throw "User not found";
+        return user;
+      });
     });
   },
   // This is a fun new syntax that was brought forth in ES6, where we can define
@@ -28,11 +69,11 @@ let exportedMethods = {
       });
     });
   },
-  addUser(username, hashedPassword, profilePicture, bio) {
+  addUser(username, password, profilePicture, bio) {
     return users().then((userCollection) => {
       let newUser = {
         username: username,
-        hashedPassword: hashedPassword,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync()),
         _id: uuid.v4(),
         profilePicture: profilePicture,
         bio: bio
@@ -45,7 +86,6 @@ let exportedMethods = {
       });
     });
   },
-  
   removeUser(id) {
     return users().then((userCollection) => {
       return userCollection.removeOne({ _id: id }).then((deletionInfo) => {
